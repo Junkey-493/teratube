6f2e2c73e6997e2e1e6dbd759ab8dea86926a6e1
Junkey-493 <56882148+Junkey-493@users.noreply.github.com>
2020-01-06 16:52:46 +0900

    watch markup

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,9 @@
 class VideosController < ApplicationController
+  before_action :find_video, only: [:show]
   before_action :move_to_index, except: [:index, :show]
+
   def index
+    @videos = Video.all.order(created_at: :DESC)
   end
 
   def new
@@ -9,7 +12,15 @@ class VideosController < ApplicationController
   def create
   end
 
+  def show
+  end
+
   private
+
+  def find_video
+    @video = Video.find(params[:id])
+  end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
