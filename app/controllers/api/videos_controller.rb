70160e2d2d6a667e76c9adaab80d8df0e2cb701b
Junkey-493 <56882148+Junkey-493@users.noreply.github.com>
2020-01-23 14:27:32 +0900

    auto-update return

--- app/controllers/api/videos_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::VideosController < ApplicationController
-  def index
-    # ajaxで送られてくる最後のメッセージのid番号を変数に代入
-    last_video_id = params[:id].to_i
-    # 取得したグループでのメッセージ達から、idがlast_message_idよりも新しい(大きい)メッセージ達のみを取得
-    @messages = Video.includes(:user).where("id > #{last_video_id}")
-  end
-end
