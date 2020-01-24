2eef8b73c300d3a6fb0f00912bcb65c8b57203d1
Junkey-493 <56882148+Junkey-493@users.noreply.github.com>
2020-01-24 12:13:44 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,3 @@
-ActiveRecord::Base.protected_environments = %w(production staging)
 require_relative 'boot'
 
 require 'rails/all'
@@ -13,6 +12,8 @@ module Teratube
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    ActiveRecord::Base.protected_environments = %w(production staging)
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
